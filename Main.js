d45187ea1fb95a2af7c57bb9a3143ce1d636f2b5
HenkBlaauw <42346551+HenkBlaauw@users.noreply.github.com>
2018-08-21 13:37:08 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -12,25 +12,7 @@ add_store_ManName : 5,
 add_store_telno : 6
 }
 
-var hardcodeStores = [
-    {
-        name : "Checkers Durbanville",
-        telno : "+27 9704",
-        adressStreet : "13 Wellington Road",
-        adressSuburb : "Durbanville",
-        adressCity : "Cape Town",
-        adressNum : "7550"
 
-    },
-    {
-        name : "Checkers Kloof Street",
-        telno : "0214805680",
-        adressStreet : "Cnr Kloof and Welterveden Street",
-        adressSuburb : "Tamboerskloof",
-        adressCity : "Cape Town",
-        adressNum : "8001"
-    }
-]
 
 
 
