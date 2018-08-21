3cf9fe778c7ca71858d75280eea8819901004d73
Henk Blaauw <hendrik@angleorange.co.za>
2018-08-21 09:06:01 +0200

    Modified Main.js

--- Main.js
+++ Main.js
@@ -15,7 +15,20 @@ add_store_telno : 6
 var hardcodeStores = [
     {
         name : "Checkers Durbanville",
-        telno : "+27 9704"
+        telno : "+27 9704",
+        adressStreet : "13 Wellington Road",
+        adressSuburb : "Durbanville",
+        adressCity : "Cape Town",
+        adressNum : "7550"
+
+    },
+    {
+        name : "Checkers Kloof Street",
+        telno : "0214805680",
+        adressStreet : "Cnr Kloof and Welterveden Street",
+        adressSuburb : "Tamboerskloof",
+        adressCity : "Cape Town",
+        adressNum : "8001"
     }
 ]
 
